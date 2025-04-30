f049b2757c504c07bd3fa7771286442cf830ddec
newrunza559 <nontapat.tit@spumail.net>
2025-04-30 22:41:13 +0700

    Add files via upload

---
+++ hotel_luna/booking/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 != password2:
+            raise ValidationError("Passwords do not match")
+        return password2
